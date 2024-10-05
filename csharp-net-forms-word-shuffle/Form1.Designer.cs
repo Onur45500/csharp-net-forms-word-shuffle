213bb2f16262d0312ec9b9426c83e70bab67ff7c
Onur45500 <onur.akmese@viacesi.fr>
2024-10-05 20:23:48 +0200

    Created the setup function

--- csharp-net-forms-word-shuffle/Form1.Designer.cs
+++ csharp-net-forms-word-shuffle/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace csharp_net_forms_word_shuffle
 {
-    partial class Form1
+    partial class GuessTheWord
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,18 +30,22 @@
         {
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
+            lblWord = new Label();
             textBox1 = new TextBox();
+            lblInfo = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(346, 127);
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.ForeColor = Color.FromArgb(0, 192, 0);
+            label1.Location = new Point(285, 26);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(200, 37);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Guess the word";
+            label1.Click += label1_Click;
             // 
             // label2
             // 
@@ -52,34 +56,46 @@
             label2.TabIndex = 1;
             label2.Text = "label2";
             // 
-            // label3
+            // lblWord
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(520, 230);
-            label3.Name = "label3";
-            label3.Size = new Size(50, 20);
-            label3.TabIndex = 2;
-            label3.Text = "label3";
+            lblWord.ForeColor = Color.White;
+            lblWord.Location = new Point(306, 112);
+            lblWord.Name = "lblWord";
+            lblWord.Size = new Size(147, 20);
+            lblWord.TabIndex = 2;
+            lblWord.Text = "label3";
+            lblWord.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(306, 230);
+            textBox1.Location = new Point(268, 176);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(217, 27);
             textBox1.TabIndex = 3;
             // 
-            // Form1
+            // lblInfo
+            // 
+            lblInfo.AutoSize = true;
+            lblInfo.Location = new Point(523, 179);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new Size(50, 20);
+            lblInfo.TabIndex = 4;
+            lblInfo.Text = "label3";
+            // 
+            // GuessTheWord
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DimGray;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblInfo);
             Controls.Add(textBox1);
-            Controls.Add(label3);
+            Controls.Add(lblWord);
             Controls.Add(label2);
             Controls.Add(label1);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "GuessTheWord";
+            Text = "Guess the word";
+            KeyPress += KeyIsPressed;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -88,7 +104,8 @@
 
         private Label label1;
         private Label label2;
-        private Label label3;
+        private Label lblWord;
         private TextBox textBox1;
+        private Label lblInfo;
     }
 }
--- csharp-net-forms-word-shuffle/Form1.cs
+++ csharp-net-forms-word-shuffle/Form1.cs
@@ -1,10 +1,41 @@
 namespace csharp_net_forms_word_shuffle
 {
-    public partial class Form1 : Form
+    public partial class GuessTheWord : Form
     {
-        public Form1()
+
+        List<string> words = new List<string>();
+        string newText;
+        int i = 0;
+        int guessed = 0;
+
+        public GuessTheWord()
         {
             InitializeComponent();
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyIsPressed(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Setup()
+        {
+            words = File.ReadLines("words.txt").ToList();
+            newText = Scramble(words[i]);
+            lblWord.Text = newText;
+            lblInfo.Text = "Words:" + (i + 1) + " of " + words.Count;
+        }
+
+        private string Scramble(string text)
+        {
+
         }
     }
 }
